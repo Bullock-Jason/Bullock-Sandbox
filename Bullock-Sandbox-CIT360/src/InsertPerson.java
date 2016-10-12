774d0d5d20af940609f78b5904e63dc62cda35c1
BULLOJP <BULLOJP@NET219539.cssd11.k12.co.us>
2016-10-11 23:14:18 -0600

    Got Hibernate to work!

---
+++ Bullock-Sandbox-CIT360/src/InsertPerson.java
@@ -0,0 +1,51 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+
+
+/**
+ *
+ * @author BULLOJP
+ */
+public class InsertPerson {
+    
+    
+     public static void main(String[] args) {
+        // This is where the code goes to insert data into the database
+       Configuration cfg = new Configuration();
+       //location of hibernation xml file with how to connect to database
+       cfg.configure("hibernate.cfg.xml");
+       SessionFactory sf = cfg.buildSessionFactory();
+       Session session = sf.openSession();
+       Transaction trans = session.beginTransaction();
+       
+       Person person = new Person();
+       person.setId(4);
+       person.setName("Jason");
+       person.setEmail("Jason@byui.edu");
+       person.setPhone("7184445454");
+       //to save to database
+       session.save(person);
+       session.flush();
+       trans.commit();
+       session.close();
+       
+       //need to figure out database settings to get this to write
+       
+       
+       
+       
+       
+       
+       
+         
+        
+    }
+}
---
+++ Bullock-Sandbox-CIT360/src/Person.java
@@ -0,0 +1,62 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+import java.io.Serializable;
+
+/**
+ *
+ * @author BULLOJP
+ */
+public class Person implements Serializable {
+ /** Database parameters are mapped through classes and variables. The database table has to be the same name as the CLASS (in this case, PERSON). The columns are mapped to variables.
+  * 
+  * These are mapped through a mapping file
+  */
+    private int id;
+    private String name;
+    private String phone;
+    private String email;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    //Also needs an empty constructor
+    public Person(){}
+    
+
+    
+    
+    
+}
