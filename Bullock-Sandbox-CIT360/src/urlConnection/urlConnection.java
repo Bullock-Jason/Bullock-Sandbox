a518964a7e2a2ca0f373d1a67262a54a47e3017c
BULLOJP <BULLOJP@NET219539.cssd11.k12.co.us>
2016-10-15 20:16:46 -0600

    Finishing touches for portfolio

--- Bullock-Sandbox-CIT360/src/urlConnection/urlConnection.java
+++ Bullock-Sandbox-CIT360/src/urlConnection/urlConnection.java
@@ -20,45 +20,30 @@ public class urlConnection {
     public static void main(String[] args) throws Exception{
                 URL Url = new URL("http://data.colorado.gov/resource/4ykn-tg5h.json?entitystatus=Good%20Standing&principalzipcode=80923");
 		
-		try{
-			HttpURLConnection urlCn = (HttpURLConnection) Url.openConnection();
-		
-                        //JAVA COLLECTION FRAMEWORK EXAMPLE INCLUDING MAP/SET
-                        //
-			//************************
-			// Collection example using Map and Set
-			//get list of header fields and a set of the header keys
-			System.out.println("\nJava Collection Framework Code Showing Map and Set:");
-			Map<String,List<String>> headMap = urlCn.getHeaderFields();
-			Set<String> urlHeader = headMap.keySet();
-			TreeMap<String,List<String>> hvtm = new TreeMap<String,List<String>>();
-			
-			System.out.println("Web Server Header details: ");
-			//display all headers and values
-			for(String theKey : urlHeader) {
-				System.out.println("Info: "+theKey+" Value: "+ headMap.get(theKey));
-				// load treeMap
-				if(theKey != null){
-					hvtm.put(theKey,headMap.get(theKey));
-				}
-			}
-			
-			//JAVA COLLECTION EXAMPLE USING TREEMAP
-                        //
-			System.out.println("\nJava Collection Framework Code Showing TreeMap:");
-	        //add key-value pair to TreeMap
-	        System.out.println(hvtm);
-			
-	        //display the contents using an iterator
-	        /* Display content using Iterator*/
-	        Set set = hvtm.entrySet();
-	        Iterator iterator = set.iterator();
-	        while(iterator.hasNext()) {
-	           Map.Entry mentry = (Map.Entry)iterator.next();
-	           System.out.print("key is: "+ mentry.getKey() + " & Value is: ");
-	           System.out.println(mentry.getValue());
-	        }
-			
+    try{
+            HttpURLConnection urlCn = (HttpURLConnection) Url.openConnection();
+
+            //JAVA COLLECTION FRAMEWORK EXAMPLE INCLUDING MAP/SET
+            //
+            //************************
+            // Map and Set code. This took awhile to get right. I referenced tutorialspoint.com for help. 
+
+            //This gets data from the HTTP header and sets keys and such
+            System.out.println("\nJava Collection Framework Code Showing Map and Set:");
+            Map<String,List<String>> headMap = urlCn.getHeaderFields();
+            Set<String> urlHeader = headMap.keySet();
+
+
+            System.out.println("Web Server Header details: ");
+            //show web server details from HTTP header.
+            //theKey is the value that it gets from the HTTP header.
+            for(String theKey : urlHeader) {
+                    System.out.println("Info: "+theKey+" Value: "+ headMap.get(theKey));
+
+            }
+
+		// Next thing to do is to integrate this with TreeMap but I don't have it done yet.
+	     	
                 }
                 catch(IOException e){
 			System.out.println("Some Error Somewhere is breaking your code. Figure it out! The site probably can't be reached right now"
