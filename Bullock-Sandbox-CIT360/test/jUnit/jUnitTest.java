fd8a2e180c73bf88a73fb350e59b22d7eaca135d
BULLOJP <BULLOJP@NET219536.cssd11.k12.co.us>
2016-11-17 18:41:22 -0700

    jUnit

---
+++ Bullock-Sandbox-CIT360/test/jUnit/jUnitTest.java
@@ -0,0 +1,184 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jUnit;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author BULLOJP
+ */
+public class jUnitTest {
+    
+    public jUnitTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+    }
+    
+    @After
+    public void tearDown() {
+    }
+
+    /**
+     * Test of testAssertArrayEquals method, of class jUnit.
+     */
+    @Test
+    public void testTestAssertArrayEquals() {
+        System.out.println("testAssertArrayEquals");
+        jUnit instance = new jUnit();
+        instance.testAssertArrayEquals();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testAssertEquals method, of class jUnit.
+     */
+    @Test
+    public void testTestAssertEquals() {
+        System.out.println("testAssertEquals");
+        jUnit instance = new jUnit();
+        instance.testAssertEquals();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testAssertFalse method, of class jUnit.
+     */
+    @Test
+    public void testTestAssertFalse() {
+        System.out.println("testAssertFalse");
+        jUnit instance = new jUnit();
+        instance.testAssertFalse();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testAssertNotNull method, of class jUnit.
+     */
+    @Test
+    public void testTestAssertNotNull() {
+        System.out.println("testAssertNotNull");
+        jUnit instance = new jUnit();
+        instance.testAssertNotNull();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testAssertNotSame method, of class jUnit.
+     */
+    @Test
+    public void testTestAssertNotSame() {
+        System.out.println("testAssertNotSame");
+        jUnit instance = new jUnit();
+        instance.testAssertNotSame();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testAssertNull method, of class jUnit.
+     */
+    @Test
+    public void testTestAssertNull() {
+        System.out.println("testAssertNull");
+        jUnit instance = new jUnit();
+        instance.testAssertNull();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testAssertSame method, of class jUnit.
+     */
+    @Test
+    public void testTestAssertSame() {
+        System.out.println("testAssertSame");
+        jUnit instance = new jUnit();
+        instance.testAssertSame();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testAssertThatBothContainsString method, of class jUnit.
+     */
+    @Test
+    public void testTestAssertThatBothContainsString() {
+        System.out.println("testAssertThatBothContainsString");
+        jUnit instance = new jUnit();
+        instance.testAssertThatBothContainsString();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testAssertThatHasItems method, of class jUnit.
+     */
+    @Test
+    public void testTestAssertThatHasItems() {
+        System.out.println("testAssertThatHasItems");
+        jUnit instance = new jUnit();
+        instance.testAssertThatHasItems();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testAssertThatEveryItemContainsString method, of class jUnit.
+     */
+    @Test
+    public void testTestAssertThatEveryItemContainsString() {
+        System.out.println("testAssertThatEveryItemContainsString");
+        jUnit instance = new jUnit();
+        instance.testAssertThatEveryItemContainsString();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testAssertThatHamcrestCoreMatchers method, of class jUnit.
+     */
+    @Test
+    public void testTestAssertThatHamcrestCoreMatchers() {
+        System.out.println("testAssertThatHamcrestCoreMatchers");
+        jUnit instance = new jUnit();
+        instance.testAssertThatHamcrestCoreMatchers();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+
+    /**
+     * Test of testAssertTrue method, of class jUnit.
+     */
+    @Test
+    public void testTestAssertTrue() {
+        System.out.println("testAssertTrue");
+        jUnit instance = new jUnit();
+        instance.testAssertTrue();
+        // TODO review the generated test code and remove the default call to fail.
+        fail("The test case is a prototype.");
+    }
+    
+}
